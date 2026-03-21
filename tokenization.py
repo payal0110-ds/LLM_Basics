e07e0eb0e8d58045bd3ed0c5c1d0d5b7b0803109
Payal Parida <payalparida@Payals-MacBook-Air.local>
2026-03-21 20:10:21 +0530

    Basic code for Tokenization.

---
+++ tokenization.py
@@ -0,0 +1,19 @@
+# Convert Paragraph/corpus to sentences
+from nltk import sent_tokenize
+
+paragraph="""My name is Payal. I'm a Data Scientist and currently working 
+in GA as a Data Scientist. My responsibilities are advance analysis, 
+creating predictive models, developing NLP such as Chatbot, RAG models."""
+
+sentences=sent_tokenize(paragraph)
+print(sentences)
+print(len(sentences))
+
+# Convert paragraph to words
+
+from nltk import word_tokenize
+
+words=word_tokenize(paragraph)
+print(words)
+print(len(words))
+
